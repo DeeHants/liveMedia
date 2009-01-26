732325205a9e6b89c67d3f182f2f7fa3b0a5c331
Ross Finlayson <finlayson@live555.com>
2009-01-26 00:00:00 +0000

    2009.01.26: - Updated the header file for "rtcp_from_spec.c" to make the call to "exit()" a noop.   (Thanks to Chris Silvers for raising this issue.) - Changed the definition of "RTSPClientSession" (in "RTSPServer") to make it protected (and also to make its   command handler functions protected and virtual), to allow developers to subclass it (to change the   implementation of some commands).   (Thanks to Yedidia Amit for suggesting this.) - Fixed a minor bug in "scheduleNextQOSMeasurement()" (in the "openRTSP" implementation).   (Thanks to Gabriele De Luca for noticing this.)
    2008.12.20:
    - Removed - from "MultiFramedRTPSource.cpp" - the historical sanity check for bizarrely delayed packets.
      Accepting such bizarro packets causes problems in the code; it's best not to accept them at all.
    - Added to "FramedFilter" a new public function "detachInputSource()" that sets the filter's input source
      to NULL.  This is useful if you want to close the filter independently of closing its input source.
      (Thanks to Alex Cichowski for this suggestion.)
    - Modified the header file for "MultiFramedRTPSink" to make its implementation of the "stopPlaying()" virtual
      function public.  (Thanks to Guy Bonneau for this suggestion.)
    - Changed "MediaSession::initiate()" to make the port number selection mechanism (for unicast streams) more robust.
      (Thanks to Pierre Ynard for this suggestion.)

--- UsageEnvironment/include/UsageEnvironment_version.hh
+++ UsageEnvironment/include/UsageEnvironment_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "UsageEnvironment" library
-// Copyright (c) 1996-2008 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2009 Live Networks, Inc.  All rights reserved.
 
 #ifndef _USAGEENVIRONMENT_VERSION_HH
 #define _USAGEENVIRONMENT_VERSION_HH
 
-#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2008.11.13"
-#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1226534400
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2009.01.26"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1232928000
 
 #endif
