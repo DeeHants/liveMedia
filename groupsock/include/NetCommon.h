7a856b2ea1973d6f9eaa8cb153e47df74471503e
Ross Finlayson <finlayson@live555.com>
2014-07-13 00:00:00 +0100

    2014.07.13: - Corrected the previous revision's change to "groupsock/NetCommon.h" to remove a definition   of "int8_t", because that's apparently already defined for Windows.   (Thanks to Victor Kulichkin for noting this.)

--- groupsock/include/NetCommon.h
+++ groupsock/include/NetCommon.h
@@ -64,7 +64,6 @@ typedef unsigned u_int32_t;
 typedef short int16_t;
 typedef unsigned short u_int16_t;
 
-typedef char int8_t;
 typedef unsigned char u_int8_t;
 
 // For "uintptr_t" and "intptr_t", we assume that if they're not already defined, then this must be
