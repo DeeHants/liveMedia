390bbb0fe9ce2d702d79247b0a1f1d9eebc763af
Ross Finlayson <finlayson@live555.com>
2020-01-22 00:00:00 +0000

    2020.01.22: - Fixed a bug in the "TLSState" constructor (some fields were not initialized properly).   (Thanks to Horst Possegger for reporting this.) - Updated the "mikeyParse" demo application to properly parse the "Key validity" section   of a "Key Data" sub-payload.

--- testProgs/mikeyParse.cpp
+++ testProgs/mikeyParse.cpp
@@ -85,7 +85,7 @@ static Boolean parseKeyDataSubPayload(u_int8_t const*& ptr, u_int8_t const* endP
   Boolean hasSalt = Type == 1 || Type == 3;
   
   fprintf(stderr, "\t\tKey Validity: %d (%s)\n", KV,
-	  KV == 0 ? "NULL" : KV == 1 ? "SPI" : KV == 2 ? "Interval" : "unknown");
+xs	  KV == 0 ? "NULL" : KV == 1 ? "SPI/MKI" : KV == 2 ? "Interval" : "unknown");
   Boolean hasKV = KV != 0;
 
   u_int16_t keyDataLen = get2Bytes(ptr);
@@ -108,7 +108,35 @@ static Boolean parseKeyDataSubPayload(u_int8_t const*& ptr, u_int8_t const* endP
   }
 
   if (hasKV) {
-    return False; // TO COMPLETE #####
+    fprintf(stderr, "\t\tKV (key validity) data:\n");
+    if (KV == 1) { // SPI/MKI
+      testSize(1);
+      u_int8_t SPILength = getByte(ptr);
+      fprintf(stderr, "\t\t\tSPI Length: %d\n", SPILength);
+
+      testSize(SPILength);
+      fprintf(stderr, "\t\t\tSPI: ");
+      for (unsigned i = 0; i < SPILength; ++i) fprintf(stderr, ":%02x", getByte(ptr));
+      fprintf(stderr, "\n");
+    } else if (KV == 2) { // Interval
+      testSize(1);
+      u_int8_t VFLength = getByte(ptr);
+      fprintf(stderr, "\t\t\tVF Length: %d\n", VFLength);
+
+      testSize(VFLength);
+      fprintf(stderr, "\t\t\tVF: ");
+      for (unsigned i = 0; i < VFLength; ++i) fprintf(stderr, ":%02x", getByte(ptr));
+      fprintf(stderr, "\n");
+
+      testSize(1);
+      u_int8_t VTLength = getByte(ptr);
+      fprintf(stderr, "\t\t\tVT Length: %d\n", VTLength);
+
+      testSize(VTLength);
+      fprintf(stderr, "\t\t\tVT: ");
+      for (unsigned i = 0; i < VTLength; ++i) fprintf(stderr, ":%02x", getByte(ptr));
+      fprintf(stderr, "\n");
+    }
   }
     
   return True;
